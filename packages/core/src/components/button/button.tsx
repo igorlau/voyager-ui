e3126667b390b7dbc222711debd7140b5648709f
igorlau <igorassislau@gmail.com>
2024-05-11 14:27:25 -0300

    chore: set core components deploy

--- packages/core/src/components/button/button.tsx
+++ packages/core/src/components/button/button.tsx
@@ -1,51 +1,73 @@
-import { forwardRef } from 'react';
-import { ButtonProps } from './button.types';
 import { cva } from 'class-variance-authority';
 import clsx, { ClassValue } from 'clsx';
+import { forwardRef } from 'react';
+import { ButtonProps } from './button.types';
 
-const buttonStyles = cva(['rounded-md','px-5', 'py-2', 'flex', 'flex-row', 'gap-2'],{
-  variants: {
-    size: {
-      sm: ['text-sm', 'px-3', 'py-1'],
-      md: ['text-md', 'px-5', 'py-2'],
-      lg: ['text-lg', 'px-7', 'py-3'],
+const buttonStyles = cva(
+  [
+    'rounded-md',
+    'px-5',
+    'py-2',
+    'flex',
+    'flex-row',
+    'gap-2',
+    'items-center',
+    'justify-center',
+  ],
+  {
+    variants: {
+      size: {
+        sm: ['text-sm', 'px-3', 'py-1'],
+        md: ['text-md', 'px-5', 'py-2'],
+        lg: ['text-lg', 'px-7', 'py-3'],
+      },
+      color: {
+        primary: [],
+        secondary: [],
+      },
+      variant: {
+        filled: [],
+        outlined: ['border-2'],
+      },
+      disabled: {
+        true: ['opacity-50', 'cursor-not-allowed'],
+        false: ['cursor-pointer'],
+      },
     },
-    color: {
-      primary: [],
-      secondary: [],
-    },
-    variant: {
-      filled: [],
-      outlined: ['border-2'],
-    },
-    disabled: {
-      true: ['opacity-50', 'cursor-not-allowed'],
-      false: ['cursor-pointer'],
-    }
+    compoundVariants: [
+      {
+        color: 'primary',
+        variant: 'filled',
+        class: ['bg-primary-main', 'text-white'],
+      },
+      {
+        color: 'primary',
+        variant: 'outlined',
+        class: [
+          'border',
+          'border-primary-main',
+          'bg-white',
+          'text-primary-main',
+        ],
+      },
+      {
+        color: 'secondary',
+        variant: 'filled',
+        class: ['bg-secondary-main', 'text-white'],
+      },
+      {
+        color: 'secondary',
+        variant: 'outlined',
+        class: [
+          'border',
+          'border-secondary-main',
+          'bg-white',
+          'text-secondary-main',
+        ],
+      },
+    ],
   },
-  compoundVariants: [
-    {
-      color: 'primary',
-      variant: 'filled',
-      class: ['bg-primary-main', 'text-white'],
-    },
-    {
-      color: 'primary',
-      variant: 'outlined',
-      class: ['border', 'border-primary-main', 'bg-white', 'text-primary-main'],
-    },
-    {
-      color: 'secondary',
-      variant: 'filled',
-      class: ['bg-secondary-main', 'text-white'],
-    },
-    {
-      color: 'secondary',
-      variant: 'outlined',
-      class: ['border', 'border-secondary-main', 'bg-white', 'text-secondary-main'],
-    }
-  ]
-});
+);
 
 function cn(...classes: ClassValue[]) {
   return clsx(classes);
@@ -65,7 +87,9 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       ...rest
     } = props;
 
-    const classNames = cn(buttonStyles({ size, color, variant, disabled, className }));
+    const classNames = cn(
+      buttonStyles({ size, color, variant, disabled, className }),
+    );
 
     return (
       <button
@@ -74,9 +98,9 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         aria-disabled={disabled}
         className={classNames}
         {...rest}>
-          {leading && <span>{leading}</span>}
-          {children}
-          {trailing && <span>{trailing}</span>}
+        {leading && <span>{leading}</span>}
+        {children}
+        {trailing && <span>{trailing}</span>}
       </button>
     );
   },
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,22 +1,27 @@
 /// <reference types='vitest' />
-import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
-import react from "@vitejs/plugin-react";
-import { glob } from "glob";
-import { fileURLToPath } from "node:url";
-import * as path from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { glob } from 'glob';
+import { fileURLToPath } from 'node:url';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: "../../node_modules/.vite/packages/core",
+  cacheDir: '../../node_modules/.vite/packages/core',
 
   plugins: [
     react(),
     nxViteTsPaths(),
     dts({
-      entryRoot: "src",
-      tsconfigPath: path.join(__dirname, "tsconfig.lib.json"),
+      entryRoot: 'src',
+      exclude: ['**/*.stories.{ts,tsx}'],
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+    }),
+    copy({
+      targets: [{ src: 'package.json', dest: '../../dist/packages/core' }],
     }),
   ],
 
@@ -28,46 +33,46 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: "../../dist/packages/core",
+    outDir: '../../dist/packages/core/src',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: "src/index.ts",
-      name: "core",
-      fileName: "index",
+      entry: 'src/index.ts',
+      name: 'core',
+      fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ["es"],
+      formats: ['es'],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       input: Object.fromEntries(
         glob
-          .sync("src/**/*.{ts,tsx}")
-          .filter((file) => !file.includes(".stories."))
+          .sync('src/**/*.{ts,tsx}')
+          .filter((file) => !file.includes('.stories.'))
           .map((file) => [
             // The name of the entry point
             // src/nested/foo.ts becomes nested/foo
             path.relative(
-              "src",
-              file.slice(0, file.length - path.extname(file).length)
+              'src',
+              file.slice(0, file.length - path.extname(file).length),
             ),
             // The absolute path to the entry file
             // src/nested/foo.ts becomes /project/src/nested/foo.ts
             fileURLToPath(new URL(file, import.meta.url)),
-          ])
+          ]),
       ),
       output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "react/jsx-runtime": "jsxRuntime",
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
         },
       },
     },
@@ -76,15 +81,15 @@ export default defineConfig({
   test: {
     globals: true,
     cache: {
-      dir: "../../node_modules/.vitest",
+      dir: '../../node_modules/.vitest',
     },
-    environment: "jsdom",
-    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
-    reporters: ["default"],
+    reporters: ['default'],
     coverage: {
-      reportsDirectory: "../../coverage/packages/core",
-      provider: "v8",
+      reportsDirectory: '../../coverage/packages/core',
+      provider: 'v8',
     },
   },
 });
